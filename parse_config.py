1589edbb4c354683870971c056aa52049eec4ef1
Tomohito Higuchi <tomgoodsun+github@gmail.com>
2016-07-01 02:32:26 +0900

    [modify] Update mysql command builder

--- parse_config.py
+++ parse_config.py
@@ -1,27 +1,44 @@
 # -*- coding: utf-8 -*-
 
-import ConfigParser, os, sys
+import ConfigParser, os, sys, getopt
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "c:e:", ["config=", "sql="])
+except getopt.GetoptError, err:
+    print str(err)
+    sys.exit(2)
+sql = None
+config_file = sys.argv[0] + "/default_settings.ini"
+for o, a in opts:
+    if o in ("-e", "--sql"):
+        sql = a
+    elif o in ("-c", "--config"):
+        config_file = a
+    else:
+        raise Exception("error")
+
+if not os.path.exists(config_file):
+    print "No config file '" + config_file + "'"
+    sys.exit(2)
 
 config = ConfigParser.SafeConfigParser()
-config.read('./default_settings.ini')
+#config.read('./default_settings.ini')
+config.read(config_file)
 sections = config.sections()
 cmds = []
 for section in sections:
-    cmd = ''
-    if config.has_option(section, 'command'):
-        cmd += config.get(section, 'command')
-    else:
-        raise Exception, 'Options "command" is required.'
+    cmd = "mysql "
     labels = config.options(section) 
     for label in labels:
-        if label == 'command':
-            continue
-        elif label == 'options':
-            cmd += ' ' + config.get(section, label)
+        if label == "options":
+            cmd += " " + config.get(section, label)
         else:
-            cmd += ' --' + label + '=' + config.get(section, label)
+            cmd += " --" + label + "=" + config.get(section, label)
+    if sql is not None:
+        cmd += " -e \"" + sql + "\""
     cmds.append(cmd)
 
 for cmd in cmds:
     print cmd
 
+# vim: set et ts=4 sts=4 sw=4 :
